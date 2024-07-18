5c10f0423d925cdcfbee1ce6f43b756e176aef65
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-07-18 14:25:21 +0700

    create api bill

---
+++ shopdecor_api/Repositories/BillRepositories/BillRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shopdecor_api.Data;
+using shopdecor_api.Models.Domain;
+
+namespace shopdecor_api.Repositories.BillRepositories
+{
+    public class BillRepository : IBillRepository
+    {
+        private readonly SeabugDbContext _db;
+
+        public BillRepository(SeabugDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<HoaDon>> GetAllAsync()
+        {
+            return await _db.HoaDon.ToListAsync();
+        }
+
+        public async Task<HoaDon?> GetByIdAsync(int id)
+        {
+            return await _db.HoaDon.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
---
+++ shopdecor_api/Repositories/BillRepositories/IBillRepository.cs
@@ -0,0 +1,10 @@
+﻿using shopdecor_api.Models.Domain;
+
+namespace shopdecor_api.Repositories.BillRepositories
+{
+    public interface IBillRepository
+    {
+        Task<IEnumerable<HoaDon>> GetAllAsync();
+        Task<HoaDon?> GetByIdAsync(int id);
+    }
+}
