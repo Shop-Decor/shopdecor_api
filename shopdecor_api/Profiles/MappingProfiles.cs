389fddb9dd47f38b0213bf8ada859711da52ded5
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-07-25 00:15:40 +0700

    update get product user

--- shopdecor_api/Profiles/MappingProfiles.cs
+++ shopdecor_api/Profiles/MappingProfiles.cs
@@ -16,7 +16,7 @@ namespace shopdecor_api.Profiles
             CreateMap<SanPham, AddProductRequest>().ReverseMap();
             CreateMap<SanPham, IndexProductRequest>().ForMember(dest => dest.Hinhs, opt => opt.MapFrom(src => src.Hinhs.Select(h => h.Link))).ReverseMap();
             CreateMap<SanPham, GetUserProduct>()
-            .ForMember(dest => dest.Colors, opt => opt.MapFrom(src => src.SanPham_ChiTiets.Select(ct => ct.MauSac.Id).Distinct().ToList()))
+            .ForMember(dest => dest.ColorName, opt => opt.MapFrom(src => src.SanPham_ChiTiets.Select(ct => ct.MauSac.TenMauSac).Distinct().ToList()))
             .ForMember(dest => dest.Hinh, opt => opt.MapFrom(src => src.Hinhs.FirstOrDefault().Link))
             .ForMember(dest => dest.gia, opt => opt.MapFrom(src => src.SanPham_ChiTiets.Any() ? src.SanPham_ChiTiets.Min(s => s.Gia) : 0)).ReverseMap();
             CreateMap<SanPham, UpdateProductRequest>().ReverseMap();
