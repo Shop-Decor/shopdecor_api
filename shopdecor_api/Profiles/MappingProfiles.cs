b93653827b90e500c63c068420631e0503117269
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-07-31 22:11:23 +0700

    update produc user

--- shopdecor_api/Profiles/MappingProfiles.cs
+++ shopdecor_api/Profiles/MappingProfiles.cs
@@ -17,12 +17,14 @@ namespace shopdecor_api.Profiles
             CreateMap<SanPham, IndexProductRequest>().ForMember(dest => dest.Hinhs, opt => opt.MapFrom(src => src.Hinhs.Select(h => h.Link))).ReverseMap();
             CreateMap<SanPham, GetUserProduct>()
             .ForMember(dest => dest.ColorName, opt => opt.MapFrom(src => src.SanPham_ChiTiets.Select(ct => ct.MauSac.TenMauSac).Distinct().ToList()))
+            .ForMember(dest => dest.Color, opt => opt.MapFrom(src => src.SanPham_ChiTiets.OrderBy(x => x.Gia).Select(x => x.MauSac.TenMauSac).FirstOrDefault()))
             .ForMember(dest => dest.Hinh, opt => opt.MapFrom(src => src.Hinhs.FirstOrDefault().Link))
             .ForMember(dest => dest.gia, opt => opt.MapFrom(src => src.SanPham_ChiTiets.Any() ? src.SanPham_ChiTiets.Min(s => s.Gia) : 0))
             .ForMember(dest => dest.LoaiGiam, opt => opt.MapFrom(x => x.KhuyenMai.LoaiGiam))
             .ForMember(dest => dest.MenhGia, opt => opt.MapFrom(x => x.KhuyenMai.MenhGia))
             .ForMember(dest => dest.Size, opt => opt.MapFrom(src => src.SanPham_ChiTiets.OrderBy(s => s.Gia).Select(s => s.KichThuoc.TenKichThuoc).FirstOrDefault()))
             .ForMember(dest => dest.SoLuong, opt => opt.MapFrom(src => src.SanPham_ChiTiets.OrderBy(s => s.Gia).Select(x => x.SoLuong).FirstOrDefault()))
+            .ForMember(dest => dest.MaGiamGia, opt => opt.MapFrom(x => x.KhuyenMai.MaGiamGia))
             .ReverseMap();
             CreateMap<SanPham, UpdateProductRequest>().ReverseMap();
 
