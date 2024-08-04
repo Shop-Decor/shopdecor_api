43697af939d8caa4b9a5383e12004ff6136788e4
Do Hoang Phong <hoang@MSIGF63THIN10SC>
2024-08-04 15:40:20 +0700

    update database

--- shopdecor_api/Controllers/OrderController.cs
+++ shopdecor_api/Controllers/OrderController.cs
@@ -6,41 +6,39 @@ using shopdecor_api.Repositories.OrderRepositories;
 namespace shopdecor_api.Controllers
 {
     [ApiController]
-	[Route("api/[controller]")]
-	public class OrderController : Controller
-	{
-		private readonly IOrderRipository _orderRipository;
-		private readonly IMapper _mapper;
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        private readonly IOrderRipository _orderRipository;
+        private readonly IMapper _mapper;
 
-		public OrderController(IOrderRipository orderRipository, IMapper mapper)
-		{
-			_orderRipository = orderRipository;
-			_mapper = mapper;
-		}
+        public OrderController(IOrderRipository orderRipository, IMapper mapper)
+        {
+            _orderRipository = orderRipository;
+            _mapper = mapper;
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> orderStatus(byte status)
-		{
-			/*	var orderstatus = await _orderRipository.GetAlloderbystatus(status);
-				var maporderDTO = _mapper.Map<List<OrderDTO>>(orderstatus);*/
-			var orderstatus = await _orderRipository.GetAlloderbystatus(status);
-			return Ok(orderstatus);
-		}
+        [HttpGet]
+        public async Task<IActionResult> orderStatus(byte status)
+        {
+            var orderstatus = await _orderRipository.GetAlloderbystatus(status);
+            return Ok(orderstatus);
+        }
 
-		[HttpPut("{id}")]
-		public async Task<IActionResult> updatestatus(int id, byte status, string? un)
-		{
-			
-			
-			var test = await _orderRipository.Updateorder(id, status, un);
-			if(test == null)
-			{
-				return BadRequest();
-			}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> updatestatus(int id, byte status, string? un)
+        {
 
-			return Ok(test);
-			
-		}
+
+            var test = await _orderRipository.Updateorder(id, status, un);
+            if (test == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(test);
+
+        }
         [HttpPost("{CreateOrder}")]
         public async Task<IActionResult> CreateOrder([FromBody] OrderCreateDto orderDto)
         {
@@ -72,6 +70,7 @@ namespace shopdecor_api.Controllers
             return Ok(order);
         }
 
-
+        //[HttpGet("user/{id}")]
+        //public Task<IActionResult> GetOrderByUser()
     }
 }
---
+++ shopdecor_api/Migrations/20240804083707_delete taikhoan.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopdecor_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletetaikhoan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DonHang_TaiKhoan_TaiKhoanId",
+                table: "DonHang");
+
+            migrationBuilder.DropTable(
+                name: "TaiKhoan");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DonHang_TaiKhoanId",
+                table: "DonHang");
+
+            migrationBuilder.DropColumn(
+                name: "TaiKhoanId",
+                table: "DonHang");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Hinh",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hinh_UserId",
+                table: "Hinh",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hinh_AspNetUsers_UserId",
+                table: "Hinh",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hinh_AspNetUsers_UserId",
+                table: "Hinh");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Hinh_UserId",
+                table: "Hinh");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Hinh");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TaiKhoanId",
+                table: "DonHang",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TaiKhoan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DiaChi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HoTen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LoaiTK = table.Column<bool>(type: "bit", nullable: false),
+                    NgayTao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SDT = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TrangThai = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaiKhoan", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DonHang_TaiKhoanId",
+                table: "DonHang",
+                column: "TaiKhoanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DonHang_TaiKhoan_TaiKhoanId",
+                table: "DonHang",
+                column: "TaiKhoanId",
+                principalTable: "TaiKhoan",
+                principalColumn: "Id");
+        }
+    }
+}
