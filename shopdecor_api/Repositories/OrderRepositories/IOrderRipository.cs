29bc7f5a9edf5a0d023b183c279a9f9fb06f348e
Ro0190124 <128989515+Ro0190124@users.noreply.github.com>
2024-08-14 11:24:05 +0700

    update donhang

--- shopdecor_api/Repositories/OrderRepositories/IOrderRipository.cs
+++ shopdecor_api/Repositories/OrderRepositories/IOrderRipository.cs
@@ -5,7 +5,7 @@ namespace shopdecor_api.Repositories.OrderRepositories
 {
     public interface IOrderRipository
     {
-        Task<IEnumerable<DonHang>> GetAlloderbystatus(byte status);
+        Task<IEnumerable<DonHang>> GetAlloderbystatus(byte? status);
         Task<DonHang?> Updateorder(int id, byte status, string? un);
         Task<DonHang?> GetorderAsync(int id);
         Task<DonHang> CreateOrderAsync(CreateOrderDTO orderDto, ApplicationUser applicationUser);
